5b4c4280826b87a152e2337ceb9c5cd72664b784
ychencs <yfchen2009@gmail.com>
2015-12-11 15:06:54 -0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+/* This file handles the content of the contact page. */
+
+require_once('repository_fns.php');
+
+do_html_header_contact();
+  
+do_page_content_contact();
+
+do_html_footer();
+
+?>
