5b58f784801981656285f0d543fc706b566e37a5
ychencs <yfchen2009@gmail.com>
2015-12-11 14:54:29 -0500

    Create add_obj_form.php

---
+++ add_obj_form.php
@@ -0,0 +1,8 @@
+<?php
+  /* This file handles the content of the learning objects upload page. */
+  
+  require_once('repository_fns.php');
+  do_html_header_member();
+  do_page_content_add_object();
+  do_html_footer();
+?>
