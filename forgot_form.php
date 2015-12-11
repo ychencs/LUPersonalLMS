282030d11d4b1e722d25b37c2fdd752563af3b9f
ychencs <yfchen2009@gmail.com>
2015-12-11 15:21:18 -0500

    Create forgot_form.php

---
+++ forgot_form.php
@@ -0,0 +1,10 @@
+<?php
+/* This file handles the page content of reset password. */
+
+ require_once('repository_fns.php');
+ do_html_header('Reset password');
+ 
+ display_forgot_form();
+
+ do_html_footer();
+?>
